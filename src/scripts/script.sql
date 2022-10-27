42e94fa9af23021b212d5ffa1f12d6184b235cc9
Lynnis <augustinesavary01@mail.com>
2022-10-27 16:41:57 +0200

    Mise à jour colonne BDD

--- src/scripts/script.sql
+++ src/scripts/script.sql
@@ -1,67 +1,67 @@
 CREATE TABLE Note(
-     id_note INT,
-     remarque VARCHAR(400),
-     dat_note DATE,
-     note_oral DECIMAL(15,2),
-     note_dossier DECIMAL(15,2),
-     note_dossier_fin DECIMAL(15,2),
-     note_oral_fin DECIMAL(15,2),
-     note_entre DECIMAL(15,2),
-     PRIMARY KEY(id_note)
+                     ID_NOT INT,
+                     REM_NOT VARCHAR(400),
+                     DAT_NOT DATE,
+                     NOT_ORA_NOT DECIMAL(15,2),
+                     NOT_DOS_NOT DECIMAL(15,2),
+                     NOT_DOS_FIN_NOT DECIMAL(15,2),
+                     NOT_ORA_FIN_NOT DECIMAL(15,2),
+                     NOT_ENT_NOT DECIMAL(15,2),
+                     PRIMARY KEY(ID_NOT)
 )ENGINE=InnoDB;
 
-CREATE TABLE Administrateur(
-   id_admin INT,
-   log_admin VARCHAR(50),
-   mdp_admin VARCHAR(50),
-   PRIMARY KEY(id_admin)
+CREATE TABLE Admin(
+                      ID_ADM INT,
+                      LOG_ADM VARCHAR(50),
+                      MDP_ADM VARCHAR(250),
+                      PRIMARY KEY(ID_ADM)
 )ENGINE=InnoDB;
 
 CREATE TABLE Entreprise(
-   id_entre INT,
-   lib_entre VARCHAR(50),
-   nom_mds VARCHAR(50),
-   prenom_mds VARCHAR(50),
-   cp_entre INT,
-   ville_entre VARCHAR(50),
-   rue_entre VARCHAR(50),
-   numero_rue_entre INT,
-   tel_mds INT,
-   mail_mds VARCHAR(50),
-   PRIMARY KEY(id_entre)
+                           ID_ENT INT,
+                           LIB_ENT VARCHAR(100),
+                           NOM_MDS_ENT VARCHAR(50),
+                           PRE_MDS_ENT VARCHAR(50),
+                           CP_ENT INT,
+                           VIL_ENT VARCHAR(50),
+                           RUE_ENT VARCHAR(50),
+                           NUM_RUE_ENT INT,
+                           TEL_MDS_ENT INT,
+                           MAI_MDS_ENT VARCHAR(50),
+                           PRIMARY KEY(ID_ENT)
 )ENGINE=InnoDB;
 
 CREATE TABLE Tuteur(
-   id_tuteur INT,
-   nom_tut VARCHAR(50),
-   prenom_tut VARCHAR(50),
-   tel_tut VARCHAR(50),
-   log_tut VARCHAR(50),
-   mdp_tut VARCHAR(50),
-   id_note INT NOT NULL,
-   PRIMARY KEY(id_tuteur),
-   FOREIGN KEY(id_note) REFERENCES Note(id_note)
+                       ID_TUT INT,
+                       NOM_TUT VARCHAR(50),
+                       PRE_TUT VARCHAR(50),
+                       TEL_TUT VARCHAR(50),
+                       LOG_TUT VARCHAR(50),
+                       MDP_TUT VARCHAR(250),
+                       ID_NOT_TUT INT NOT NULL,
+                       PRIMARY KEY(ID_TUT),
+                       FOREIGN KEY(ID_NOT_TUT) REFERENCES Note(ID_NOT)
 )ENGINE=InnoDB;
 
 CREATE TABLE Etudiant(
-     id_etu VARCHAR(50),
-     prenom_etu VARCHAR(50),
-     nom_etu VARCHAR(50),
-     classe VARCHAR(50),
-     specialite VARCHAR(50),
-     log_etu VARCHAR(50),
-     mdp_etu VARCHAR(250),
-     cp_etu INT,
-     rue_etu VARCHAR(50),
-     ville_etu VARCHAR(50),
-     numero_rue_etu VARCHAR(50),
-     mail_etu INT,
-     tel_etu INT,
-     id_entre INT,
-     id_note INT,
-     id_tuteur INT,
-     PRIMARY KEY(id_etu),
-     FOREIGN KEY(id_entre) REFERENCES Entreprise(id_entre),
-     FOREIGN KEY(id_note) REFERENCES Note(id_note),
-     FOREIGN KEY(id_tuteur) REFERENCES Tuteur(id_tuteur)
+                         ID_ETU VARCHAR(50),
+                         PRE_ETU VARCHAR(50),
+                         NOM_ETU VARCHAR(50),
+                         CLA_ETU VARCHAR(50),
+                         SPE_ETU VARCHAR(50),
+                         LOG_ETU VARCHAR(50),
+                         MDP_ETU VARCHAR(250),
+                         CP_ETU INT,
+                         RUE_ETU VARCHAR(50),
+                         VIL_ETU VARCHAR(50),
+                         NUM_RUE_ETU VARCHAR(50),
+                         MAI_ETU INT,
+                         TEL_ETU INT,
+                         ID_ENT_ETU INT,
+                         ID_NOT_ETU INT,
+                         ID_TUT_ETU INT,
+                         PRIMARY KEY(ID_ETU),
+                         FOREIGN KEY(ID_ENT_ETU) REFERENCES Entreprise(ID_ENT),
+                         FOREIGN KEY(ID_NOT_ETU) REFERENCES Note(ID_NOT),
+                         FOREIGN KEY(ID_TUT_ETU) REFERENCES Tuteur(ID_TUT)
 )ENGINE=InnoDB;
