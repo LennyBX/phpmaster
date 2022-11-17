7884dd98d7196bb4ee11a0f0f467245b1da8cbe3
lenny69200 <l.bondoux@lyon.asso.ort.fr>
2022-11-17 13:41:34 +0100

    Ajout page et design css #Lenny

---
+++ src/view/Accueil.php
@@ -0,0 +1,113 @@
+<?php
+?>
+<html lang="fr">
+
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+
+    <title>Accueil FSI</title>
+    <meta content="" name="description">
+    <meta content="" name="keywords">
+
+    <!-- Font Awesome -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet"/>
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" rel="stylesheet"/>
+    <!-- MDB -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/5.0.0/mdb.min.css" rel="stylesheet"/>
+    <link href="/public/css/main.css" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/e3c4ffb28a.js" crossorigin="anonymous"></script>
+</head>
+<body>
+
+<nav class="NavbarItems">
+    <img  class="navbar-logo" src="images/FSI_logo.png">
+    <ul class="nav-menu">
+        <li>
+            <a  class="nav-link" href="A.html"><i class="fa-solid fa-house-user"></i>
+                Accueil
+            </a>
+        <li>
+            <a  class="nav-link" href="L.html"><i class="fa-solid fa-list"></i>
+                Liste Etudiant
+            </a>
+        </li>
+
+        <li>
+            <a class="nav-link" href="/"><i class="fa-solid fa-right-to-bracket"></i>
+                Deconnexion
+            </a>
+        </li>
+    </ul>
+</nav>
+
+<div class="main">
+
+    <div class="card">
+        <div class="imgBX">
+            <img src="images/194935.png">
+        </div>
+        <div class="content-card">
+            <div class="detail-card">
+                <h2> NOM.PRENOM </h2>
+                <br>
+                <span> Profession</span>
+                <br>
+                <span> tel :</span>
+                <br>
+                <span> Residence :</span>
+            </div>
+        </div>
+    </div>
+
+    <div class="card2">
+        <div class="imgBX2">
+            <img src="images/etudiant-removebg.png">
+        </div>
+        <div class="ecris-card2">
+            <H2>Ma liste D'Etudiants</H2>
+        </div>
+
+        <div class="content-card2">
+            <span>Voici la liste de tout les étudiant que vous avez a votre responsabilitez </span>
+        </div>
+
+        <a class="buttoncard2" style="--clr:#499c54" href="L.html"><span> Cliquez-ici</span></a>
+    </div>
+
+    <div class="card3">
+        <div class="imgBX3">
+            <img src="images/services-parametres-et-icone-d-engrenage-vert-removebg-preview.png">
+        </div>
+        <div class="ecris-card3">
+            <H2>Mes Parametres</H2>
+        </div>
+
+        <div class="content-card3">
+            <span>Voici la liste de tout les étudiant que vous avez a votre responsabilitez </span>
+        </div>
+        <a class="buttoncard3" style="--clr:#499c54" href="#"><span> Cliquez-ici</span></a>
+    </div>
+</div>
+
+
+<footer class="Footeritems_2">
+    <img  class="footer-logo_2" src="images/FSI_logo.png">
+    <ul class="footer-menu_2">
+        <li>
+            <a  class="nav-link" href="A.html"><i class="fa-solid fa-house-user"></i>
+                Mention légales
+            </a>
+
+    </ul>
+</footer>
+
+</body>
+<script src="js/jquery-3.3.1.min.js"></script>
+<script src="js/popper.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/main.js"></script>
+</body>
+</html>
